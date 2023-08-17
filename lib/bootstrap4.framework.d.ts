942ea5b36b8054d999ba87afcc784c372ba1cb5a
zahmo <52087831+zahmo@users.noreply.github.com>
2023-08-17 09:49:26 +0000

    Update dist_bootstrap4_v0.8.0 to output generated at 4e36df7

---
+++ lib/bootstrap4.framework.d.ts
@@ -0,0 +1,11 @@
+import { Framework } from '@ajsf/core';
+import { Bootstrap4FrameworkComponent } from './bootstrap4-framework.component';
+import * as i0 from "@angular/core";
+export declare class Bootstrap4Framework extends Framework {
+    name: string;
+    framework: typeof Bootstrap4FrameworkComponent;
+    stylesheets: string[];
+    scripts: string[];
+    static ɵfac: i0.ɵɵFactoryDeclaration<Bootstrap4Framework, never>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<Bootstrap4Framework>;
+}
