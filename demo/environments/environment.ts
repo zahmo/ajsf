6343e0492d7eed6e645fbd1afb69837e485d90e5
zahmo <zuser@nowhere.nw>
2024-02-13 06:28:17 +0200

    feat:display package info at bottom of page

--- demo/environments/environment.ts
+++ demo/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = { production: false };
+
+    export const environment ={"production":false,"version":"17.0.0-alpha.0","angularVersion":"17.1.3","materialVersion":"17.1.2"}
+    
