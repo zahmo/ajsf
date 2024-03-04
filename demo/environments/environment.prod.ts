196107ba90e51af798f7c0b7be395c9eecb0eeca
zahmo <zuser@nowhere.nw>
2024-03-04 22:35:23 +0200

    build:auto build update of environment files with angular version info

--- demo/environments/environment.prod.ts
+++ demo/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 
-    export const environment ={"production":true,"version":"17.0.0-alpha.0","angularVersion":"17.1.3","materialVersion":"17.1.2"}
+    export const environment ={"production":true,"version":"16.2.0","angularVersion":"16.2.4","materialVersion":"16.2.2"}
     
