8c694b369158ff94410a39cf574df48140ef1d74
zahmo <zuser@nowhere.nw>
2024-03-22 14:26:29 +0200

    build:version info updated in environments

--- demo/environments/environment.prod.ts
+++ demo/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 
-    export const environment ={"production":true,"version":"17.2.3","angularVersion":"17.2.3","materialVersion":"17.2.1","cssClassPrefix":true}
+    export const environment ={"production":true,"version":"17.2.4","angularVersion":"17.2.3","materialVersion":"17.2.1","cssClassPrefix":true}
     
